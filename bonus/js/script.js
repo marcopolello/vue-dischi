c6e0c22e3f63cf4cddd81ae554ffe42bdf237e49
marcopolello <marco-pol@hotmail.it>
2020-12-01 17:16:34 +0100

    bonus ok

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -2,20 +2,36 @@ var app = new Vue({
   el: '#app',
   data: {
     listaDischi: [],
+    genere: '',
   },
   mounted: function () {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(risposta => {
       //console.log(risposta); //elemento chiamato completo
-      //console.log(risposta.data.response); //array dei dischi
-      const arrayDischi = risposta.data.response
-      for(var key in arrayDischi) {
+      console.log(risposta.data.response); //array dei dischi
+      let arrayDischi = risposta.data.response;
+      for (var i = 0; i < arrayDischi.length; i++) {
+        arrayDischi[i].visible = true;
+      } //ciclato l'array per mettere nuova proprietà
+      for(let key in arrayDischi) {
         this.listaDischi.push(arrayDischi[key]);
-      }
+      } //ciclato oggetti dentro l'array per salvarmeli
       //console.log(this.listaDischi);
     });
   },
   methods:{
+    cercaGenere() {
+      this.listaDischi.visible = false;
+      this.listaDischi.forEach((item, i) => {
+        let gen = item.genre;
+        gen = gen.toLowerCase();
+        if (gen.includes(this.genere)) {
+          item.visible = true;
+        } else {
+          item.visible = false;
+        }
+      });
 
+    }
   }
 });
