46710968ea185b445c34cfee2f781637646303ac
marcopolello <marco-pol@hotmail.it>
2020-12-01 15:57:08 +0100

    stampa a schermo del poster e titolo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    
+    listaDischi: [],
   },
   mounted: function () {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(risposta => {
+      //console.log(risposta); //elemento chiamato completo
+      //console.log(risposta.data.response); //array dei dischi
+      const arrayDischi = risposta.data.response
+      for(var key in arrayDischi) {
+        this.listaDischi.push(arrayDischi[key]);
+      }
+      console.log(this.listaDischi);
+    });
+  },
+  methods:{
 
   }
 });
