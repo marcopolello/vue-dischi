346dc9659a8bd5989bf60a800b6d3376002350cb
marcopolello <marco-pol@hotmail.it>
2020-12-02 09:52:32 +0100

    tolto ciclo inutile

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -8,15 +8,13 @@ var app = new Vue({
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(risposta => {
       //console.log(risposta); //elemento chiamato completo
-      console.log(risposta.data.response); //array dei dischi
+      //console.log(risposta.data.response); //array dei dischi
       let arrayDischi = risposta.data.response;
       for (var i = 0; i < arrayDischi.length; i++) {
         arrayDischi[i].visible = true;
       } //ciclato l'array per mettere nuova proprietà
-      for(let key in arrayDischi) {
-        this.listaDischi.push(arrayDischi[key]);
-      } //ciclato oggetti dentro l'array per salvarmeli
-      //console.log(this.listaDischi);
+
+      this.listaDischi = arrayDischi; //mi salvo direttamente tutti i dati ricevuti dalla chiamata
     });
   },
   methods:{
